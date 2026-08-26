708b605677d5d4480e7b589c0910e5150c9dfeaf
Abhay02-code <abhayaditya305@gmail.com>
2026-08-26 12:13:47 +0530

    Solved: Shortest and Lexicographically Smallest Beautiful String (leetcode)

---
+++ leetcode/Medium/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,53 @@
+// Problem: Shortest and Lexicographically Smallest Beautiful String
+// Platform: leetcode
+// Rating/Difficulty: Medium
+// Language: cpp
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/shortest-and-lexicographically-smallest-beautiful-string/
+// Solved on: 2026-08-26T06:43:45.361Z
+
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.size();
+
+        int left = 0;
+        int ones = 0;
+
+        string ans = "";
+
+        for (int right = 0; right < n; right++) {
+
+            
+            if (s[right] == '1') {
+                ones++;
+            }
+
+            
+            while (ones > k) {
+                if (s[left] == '1') {
+                    ones--;
+                }
+                left++;
+            }
+
+            while (ones == k && s[left] == '0') {
+                left++;
+            }
+
+           
+            if (ones == k) {
+                string curr = s.substr(left, right - left + 1);
+
+                if (ans == "" ||
+                    curr.size() < ans.size() ||
+                    (curr.size() == ans.size() && curr < ans)) {
+                    
+                    ans = curr;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
